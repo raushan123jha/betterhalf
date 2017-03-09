120f120fb4ad65530b3f0a098ff8b7266d88193e
steptostep93 <steptostep93@gmail.com>
2017-03-09 10:41:25 -0800

    Update index.php

--- index.php
+++ index.php
@@ -109,10 +109,10 @@
   Below we include the Login Button social plugin. This button uses
   the JavaScript SDK to present a graphical Login button that triggers
   the FB.login() function when clicked.
--->
+
 
 <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-</fb:login-button>
+</fb:login-button>-->
 
 <div id="status">
 </div>
