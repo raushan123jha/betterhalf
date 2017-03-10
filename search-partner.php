90cb0b5a5125c63394478c18f961259617bd0af3
jyoti pandey <jyotipandey631.jp@gmail.com>
2017-03-10 15:33:35 +0530

    Update search-partner.php

--- search-partner.php
+++ search-partner.php
@@ -1,11 +1,13 @@
 <html>
+<html>
 <title>
 BETTER HALF
 </title>
-<link href="css/drop.css" rel="stylesheet" type="text/css" />
 <head>
-
+<link href="css/drop.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="set_preference.css">
 </head>
+<div id="final">
 <body bgcolor="white" onload="startTime();">
 <table border-spacing="40px 10px">
 <tr><td colspan="2"><hr color="maroon" size="15px"></td></tr>
@@ -36,14 +38,92 @@ BETTER HALF
 
 
 
+<div id="div1">
+<a href="index.php"><img src="img/home.png" height="100" width="100"></a>
+ 
+ </div>
 
+<div id="contact-form">
 
+	<div id="f1">
+	
+		<br><center><h1><font color="#CA2E1E">BETTER HALF</font></h1></center> 
+		<h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		just one step away to find your "better&nbsp; half" !!! 
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4> <br>
+	</div>
+		<div id="div2">
+<a href="home.html"><img src="img/2.png" height="100" width="80"></a>
 
+ </div>
 
-
-
-
-
-
+		   <form method="post" action="new.html">
+			<div>
+		    
+		      	<br><span class="required">Location: *</span> 
+		      	<input type="text" id="name" name="name" value="" placeholder="partner's location" required="required" tabindex="1" autofocus="autofocus" />
+		     
+			</div>
+			<div>
+		    
+		      	<span class="required">Qualification: </span>
+		      	<input type="text" id="qualification" name="qualification" value="" placeholder="partner's qualification" tabindex="2" />
+		      
+		      </div>
+		      	<div>
+		   
+		      	<span class="required">Profession: </span>
+		      	<input type="text" id="profession" name="profession" value="" placeholder="partner's Profession" tabindex="2" />
+		    
+		      </div>
+		      <div>
+		    
+		      	<span class="required">Income: </span>
+		      	<input type="text" id="income" name="income" value="" placeholder="partner's income" tabindex="2"/>
+		    
+		      <div>
+		    
+		      	<span class="required">Cast: </span>
+		      	<input type="text" id="cast" name="cast" value="" placeholder="partner's cast" tabindex="2"/>
+		      </div>
+		      <div>
+		     
+		      	<span class="required">Color: </span>
+		      	<input type="text" id="color" name="color" value="" placeholder="partner's color" tabindex="2"/>
+		      </div>
+		     
+			<div>		          
+		    
+		      <span class="required">Marital Status: </span>
+			      <select id="marital_status" name="marital_status" tabindex="4">   
+					  <option value="hello"></option>
+			         <option value="hello">Single</option>
+			          <option value="hello">Divorced</option>
+			           <option value="hello">Widower</option>
+			         <option value="quote">Widow</option>  
+			        
+			        
+			      </select>
+		  
+			</div>
+			<div>		          
+		    
+		      <span class="required">Height(In Foot): </span>
+			      <select id="subject" name="subject" tabindex="4"> 
+					  <option value=""></option>
+			         <option value="foot">4</option> 
+			         <option value="foot">4.5</option> 
+			         <option value="foot">5</option> 
+			         <option value="foot">5.5</option>
+			         <option value="foot">6</option> 
+			         <option value="foot">6.5</option> 
+			         <option value="foot">7</option>  
+			      </select>
+			</div>
+			<div>		           
+		      <button name="submit" type="submit" id="submit" >Search Partner</button> <br> 
+			</div>
+		   </form>
+		   </div>
 </body>
 </html>
