826a22f92696a7af82ed9b5550355031bc87ac6a
jyoti pandey <jyotipandey631.jp@gmail.com>
2017-03-10 15:40:02 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>BETTER HALF</title>
+
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/theme.css" rel="stylesheet">
+</head>
+<body>
+	<!--header-->
+    <div class="header">
+	<div class="follow_us">
+                               <ul>
+                                   <li><a href="http://www.facebook.com" class="facebook">Facebook</a></li>
+                                    <li><a href="http://www.vimeo.com" class="vimeo">Vimeo</a></li>
+                                    <li><a href="http://www.tumbrl.com" class="tumbrl">Tumbrl</a></li>
+                                    <li><a href="http://www.twitter.com" class="twitter">Twitter</a></li>
+                                    <li><a href="http://www.del.icio.us" class="delicious">Delicious</a></li>
+                                </ul>
+                            </div></div>
+    	<div class="wrap">
+            	<div class="container">
+                    <div class="row">
+                        <div class="span4">
+                        	<div class="logo"><a href="index.php"><img src="img/logo.png" alt=""/></a></div>                        
+                        </div>
+                        <div class="span8">
+                        	
+                            <nav id="main_menu">
+                                <div class="menu_wrap">
+                                    <ul class="nav sf-menu">
+                                      <li class="current"><a href="index.php">Home</a></li>
+                                      <li><a href="about.php">About</a></li>
+                                     
+                                      <li><a href="contact.php">Contacts</a></li>									  
+                                      <li class="sub-menu"><a href="javascript:{}"><img src="img/memberlb.png" width="200" height="100"></a>
+                                           <ul>
+                                              <li><a href="sign-in-google.php"><img src="img/gllb.png" width="300" height="100"></a></li>
+                                              <li><a href="#"><img src="img/fblb.png" width="300" height="100"></a></li>											  
+                                          </ul>
+                                      </li>
+
+                                    </ul>
+                                </div>
+                             </nav>                            
+                        </div>
+                    </div>                
+             </div>
+        </div>    
+    </div>
+    <!--//header-->    
+     <hr color="maroon" size="6px">
+          <!--slider-->
+        <div id="main_slider">
+            <div class="camera_wrap" id="camera_wrap_1">
+                <div data-src="img/slider/3.jpg"></div>
+                <div data-src="img/slider/2.jpg"></div>
+                <div data-src="img/slider/1.jpg"></div>                                        
+            </div>
+            <div class="clear"></div>	
+        </div>        
+        <!--//slider-->
+                       
+        <!--space-->
+        <div class="wrap planning">
+            <div class="container">
+                <!--space between slide and thought-->
+            </div>
+        </div>
+        <!--//space-->
+        <!--thought-->
+        <div class="wrap block">
+            <div class="container welcome_block">
+            	<div class="welcome_line welcome_t"></div><p align="left">
+            	Better Half is a pioneer in the matrimonial space and has helped  Brides and  Grooms find each other to tie the holy bond of marriage.
+
+In India, the marriage ceremonies may include an elaborate ritual ,which means a contract between bride and groom.<br>
+ 
+<br>You can also find profiles from different religions Hindu, Muslim, Christian, Muslim Sunni, Muslim Shia, Christian - Catholic, Christian - Protestant, Christian - orthodox, Jain, Inter Religion, Sikh and even profiles with no particular religious belief can be found.
+
+<br><br>One can also browse by caste; Nair, Ezhava, Christian-Roman Catholic, Thiyya, Vishwakarma, Muslim-Shafi, SC and Muslim-Mapila are some of the castes that can be found here.
+
+<br>At Better Half you can search for your life partner from over one lakh mobile-verified profiles.</p>
+                <div class="welcome_line welcome_b"></div>
+            </div>
+        </div>
+    <!--footer-->
+   
+  <div id="footer">
+  
+<div id="templatemo_footer_wrapper">
+<div id="templatemo_footer_panel" style="color:#F9F7F6">
+    BETTER HALF |  by AVNISH AGRAHARI,RAUSHAN JHA,MOHD ARSH,YOGENDRA,JYOTI PANDEY
+    <div class="margin_bottom_20"></div>
+	</div> 
+</div>
+    </div>
+
+
+
+    <!--//footer-->    
+
+		<script src="js/demo.js"></script>
+           <script type="text/javascript" src="js/camera.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/superfish.js"></script>
+     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript">
+		$(document).ready(function(){	
+			//Slider
+			$('#camera_wrap_1').camera();												
+		});		
+	</script>
+</body>
+</html>
+
