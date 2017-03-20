56dd2d428a359b960576b5424f6ebcc95e84ff1f
mohdarsh1786 <0786mohdarsh@gmail.com>
2017-03-20 23:49:29 +0530

    Add files via upload

---
+++ profile_class.php
@@ -0,0 +1,22 @@
+<?php
+  include('connection.php');
+ class profile
+ {
+	 public $name,$profile_picture,$marital_status,$cast,$community,$qualification,$income,$professtion,
+	 $location,$dob,$gender,$color,$height,$serial_no;
+	 public function set_profile($conn){}
+	 
+	 
+	 //public function view_another_profile($conn){}
+	 
+	 public function edit_profile($conn){}
+	 
+	 public function send_request($conn){}
+	 
+	 public function view_own_profile($conn){}
+
+}
+
+
+
+?>
