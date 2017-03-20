8ee4ad49ec9d415584f77a2bedc20cab772cf474
Avnish Agr <avnishagr@gmail.com>
2017-03-20 14:51:38 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-
-
-
-<!-- ######################################################-->
+<?php 
+include('google_login.php');
+?>
+<!-- ############################Home###########################-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,111 +13,8 @@
 <link href="css/theme.css" rel="stylesheet">
 </head>
 <body>
-<!-- ##################login ###################-->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1870014203236364";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!--##########################Second FB login ####################################-->
-<script>
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '{your-app-id}',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      document.getElementById('status').innerHTML =
-        'Welcome to Better-Half, ' + response.name + '!'
-    });
-  }
-</script>
-
-<!--
-  Below we include the Login Button social plugin. This button uses
-  the JavaScript SDK to present a graphical Login button that triggers
-  the FB.login() function when clicked.
-
-
-<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-</fb:login-button>-->
-
-<div id="status">
-</div>
-<!-- ##########################FB login Closed #############################-->
-
 	<!--header-->
-     <div class="header">
+    <div class="header">
 	<div class="follow_us">
                                 <ul>
                                    <li><a href="www.facebook.com" class="facebook">Facebook</a></li>
@@ -132,9 +29,15 @@
             	<div class="container">
                     <div class="row">
                         <div class="span4">
-                        	<div class="logo"><a href="index.php"><img src="img/logo.png" alt=""/></a></div>                        
+                        	<div class="logo"><a href="index.php"><img src="img/logo.png" alt=""/></a></div>   <?php $_SESSION['email_id']='akansha@rediffmail.com';
+								$_SESSION['first_name'] = "Akansha";
+								$_SESSION['last_name'] = "Singh";
+							
+							
+							echo "<font color='green'> Welcome: " .$_SESSION['first_name']." ".$_SESSION['last_name']."</font>" ?>                      
                         </div>
                         <div class="span8">
+                        	
                             <nav id="main_menu">
                                 <div class="menu_wrap">
                                     <ul class="nav sf-menu">
@@ -144,10 +47,11 @@
                                       <li><a href="contact.php">Contacts</a></li>									  
                                       <li class="sub-menu"><a href="javascript:{}"><img src="img/memberlb.png" width="200" height="100"></a>
                                            <ul>
-                                              <li><a href="user-portal.php"><img src="img/gllb.png" width="300" height="100"></a></li>
-                                            <!--  <li><a href="user-portal.php"><img src="img/fblb.png" width="300" height="100"></a></li>-->											  
-                                          <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>
-					</ul>
+                                              <li><!--a href="sign-in-google.php"><img src="img/gllb.png" width="300" height="100"></a></li-->
+											  <a rel="nofollow" href=<?php echo $output; ?> class="external-link">
+											  <i class="glyphicon glyphicon-lock"></i><img src="img/gllb.png" width="300" height="100"></a>
+                                              <li><a href="user_portal.php"><img src="img/fblb.png" width="300" height="100"></a></li>											  
+                                          </ul>
                                       </li>
 
                                     </ul>
@@ -211,5 +115,3 @@
 	</script>
 </body>
 </html>
-
-
