daac9909bcc85e74f54c943ede6431a4e6ce5607
raushan <20raushannitian@gmail.com>
2017-03-24 17:58:31 +0530

    comment added

--- profile_class.php
+++ profile_class.php
@@ -1,21 +1,23 @@
+<!-- here class and method is declared and all oops concept in whole project done  by raushan and arsh -->
 <?php
-  include('connection.php');
- class profile
- {
-	 public $name,$profile_picture,$marital_status,$cast,$community,$qualification,$income,$professtion,
-	 $location,$dob,$gender,$color,$height,$serial_no;
-	 public function set_profile($conn){}
+  	include('connection.php');
+  	// here class profile is created and all method's declaration part is defined below//
+ 	class profile
+ 	{
+		 public $name,$profile_picture,$marital_status,$cast,$community,$qualification,$income,$professtion,
+	 			$location,$dob,$gender,$color,$height,$serial_no;
+	 	 public function set_profile($conn){}                   //declaration of methods and varialble of profile class//
 	 
 	 
 	 //public function view_another_profile($conn){}
 	 
-	 public function edit_profile($conn){}
+	    public function edit_profile($conn){}
 	 
-	 public function send_request($conn){}
+	    public function send_request($conn){}
 	 
-	 public function view_own_profile($conn){}
+	    public function view_own_profile($conn){}
 
-}
+	}
 
 
 
